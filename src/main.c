a4000e736d7c0d2dd8810de729736e16a5bddb7b
Pablo Marcelo Nacusse <pablo.nacusse@gmail.com>
2022-09-09 08:51:27 -0300

    Primera versión del proceso

--- src/main.c
+++ src/main.c
@@ -40,7 +40,8 @@
 
 /* === Headers files inclusions =============================================================== */
 
-#include "chip.h"
+#include "chip.h" 
+#include "digital.h"
 #include <stdbool.h>
 
 /* === Macros definitions ====================================================================== */
@@ -162,11 +163,14 @@ int main(void) {
     Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
     Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, TEC_4_GPIO, TEC_4_BIT, false);
 
+    digital_output_t led_azul = DigitalOutputCreate(LED_B_GPIO, LED_B_BIT);
     while (true) {
         if (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_1_GPIO, TEC_1_BIT) == 0) {
-            Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_B_GPIO, LED_B_BIT, true);
+            DigitalOutputActivate(led_azul);
+            //Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_B_GPIO, LED_B_BIT, true);
         } else {
-            Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_B_GPIO, LED_B_BIT, false);
+            DigitalOutputDeactivate(led_azul);
+            //Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_B_GPIO, LED_B_BIT, false);
         }
 
         current_state = (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_2_GPIO, TEC_2_BIT) == 0);
